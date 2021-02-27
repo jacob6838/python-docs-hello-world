40647d9ae1e2b82ac329c2228d9bbd663a06b497
jacob6838 <51723791+jacob6838@users.noreply.github.com>
2021-02-26 22:26:43 -0700

    created simple question page

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+
+from flask import Flask, render_template, session, request, redirect, url_for
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return redirect(url_for("survey"))
+
+
+@app.route("/survey")
+def survey():
+    return render_template('no_question.html', question="What will you choose?", option_1="Let Shinto salvage the pod", option_2="Retain ownership of the pod")
